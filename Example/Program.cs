4fb0b3a55c19ed01e68b743ad5b1bcf11ba1288c
José Antonio Collado Araque <jose.collado@plainconcepts.com>
2026-03-16 12:38:25 +0100

    Little update

--- Example/Program.cs
+++ Example/Program.cs
@@ -8,6 +8,25 @@ internal unsafe static class Program
 {
     const string IonAccessToken = "";
 
+    public static CesiumViewState createViewState()
+    {
+        CesiumEllipsoid wgs84 = CesiumAPI.EllipsoidWgs84();
+        CesiumCartographic cam = CesiumAPI.CartographicFromDegrees(-74.006, 40.7128, 1000.0); // New york city at 1000m height
+
+        CesiumVec3 position = CesiumAPI.EllipsoidCartographicToCartesian(wgs84, cam);
+
+        CesiumVec3 direction = new CesiumVec3
+        {
+            x = -position.x,
+            y = -position.y,
+            z = -position.z
+        };
+
+        CesiumVec3 up = CesiumAPI.EllipsoidGeodeticSurfaceNormalCartesian(wgs84, position);
+        CesiumVec2 viewport = new CesiumVec2 { x = 1920, y = 1080 };
+        return CesiumAPI.ViewStateCreatePerspective(position, direction, up, viewport, 45.0, 45.0, wgs84);
+    }
+
     public static void Main(string[] args)
     {
         Console.WriteLine("Starting CesiumNative...");
@@ -34,6 +53,10 @@ internal unsafe static class Program
             Console.WriteLine($"Failed to create tileset: {errorMessage}");
             return;
         }
+
+        CesiumViewState state = createViewState();
+
+        CesiumAPI.TilesetUpdateView(tileset, (nint*)&state, 1, 0.016f);
 
         // Wait for the tileset to load. In a real application, you would typically want to do this in a separate thread and render the tileset as it loads.
         Console.WriteLine("Loading tile...");
