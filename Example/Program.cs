2365c849e0963e24ac21220368ba4b159d7b7e9e
José Antonio Collado Araque <jose.collado@plainconcepts.com>
2026-03-17 15:08:28 +0100

    Test sample

--- Example/Program.cs
+++ Example/Program.cs
@@ -7,12 +7,17 @@ internal unsafe static class Program
 {
     const string IonAccessToken = "";
 
-    public static CesiumViewState createViewState()
+    public static CesiumViewState createViewState(CesiumVec3 worldCoordinates)
     {
         CesiumEllipsoid wgs84 = CesiumEllipsoid.Wgs84();
-        CesiumCartographic cam = CesiumCartographic.FromDegrees(-74.006, 40.7128, 1000.0);
+        CesiumVec3 pos = wgs84.CartographicToCartesian(new CesiumCartographic
+        {
+            longitude = -71.8711433798,
+            latitude = 41.06701636,
+            height = 0.0
+        });
 
-        CesiumVec3 position = wgs84.CartographicToCartesian(cam);
+        CesiumVec3 position = worldCoordinates;
 
         CesiumVec3 direction = new CesiumVec3
         {
@@ -53,18 +58,43 @@ internal unsafe static class Program
             return;
         }
 
-        CesiumViewState state = createViewState();
-        tileset.UpdateView(&state, 1, 0.016f);
 
         // Wait for the tileset to load.
-        Console.WriteLine("Loading tile...");
+        Console.WriteLine("Loading root tile...");
         while (!tileset.IsRootTileAvailable())
         {
             asyncSystem.DispatchMainThreadTasks();
             Thread.Sleep(10);
         }
 
-        Console.WriteLine("Tile loaded successfully!");
+        Console.WriteLine("Root tile loaded successfully!");
+        CesiumVec3 initialPos = new CesiumVec3 { x = 1333698, y = -465184, z = 4138241 };
+        CesiumVec3 lastPos = new CesiumVec3 { x = 1494255, y = -4579991, z = 4165880 };
+
+        for (int i = 0; i <= 200; i++)
+        {
+            CesiumVec3 interpolatedPos = new CesiumVec3
+            {
+                x = initialPos.x + (lastPos.x - initialPos.x) * i / 200,
+                y = initialPos.y + (lastPos.y - initialPos.y) * i / 200,
+                z = initialPos.z + (lastPos.z - initialPos.z) * i / 200
+            };
+            CesiumViewState state = createViewState(initialPos);
+            CesiumViewUpdateResult result = tileset.UpdateView(&state, 1, 0.2f);
+            Console.WriteLine($"Camera: Position=({interpolatedPos.x}, {interpolatedPos.y}, {interpolatedPos.z})");
+            Console.WriteLine($"Frame number: {result.FrameNumber}");
+            Console.WriteLine($"Number of tiles to render: {result.TilesToRenderCount}");
+            Console.WriteLine($"TilesVisited: {result.TilesVisited}");
+            Console.WriteLine($"TilesCulled: {result.TilesCulled}");
+            Console.WriteLine($"MaxDepth: {result.MaxDepthVisited}");
+            for (int j = 0; j < result.TilesToRenderCount; j++)
+            {
+                CesiumTile tile = result.GetTileToRender(j);
+                Console.WriteLine($"  Tile {j}: LoadState={tile.LoadState}, GeometricError={tile.GeometricError}, HasRenderContent={tile.HasRenderContent()}, BoundingVolume={tile.BoundingVolume.volume.region}");
+            }
+            Console.WriteLine("----------------------------------------------------------------");
+            Thread.Sleep(200);
+        }
     }
 
     private static void ErrorCallback(void* userData, byte* message)
